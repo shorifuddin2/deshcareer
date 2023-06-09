4ec9f0e7b638d4a0b6e2dbe3e2adef2ff6e787c5
Shorif Uddin <shorifuddin240915@gmail.com>
2023-06-10 02:19:24 +0600

    naverbar complete

--- src/Home/Header.jsx
+++ src/Home/Header.jsx
@@ -1,47 +1,41 @@
-
+import Logo from "../assets/short-logo.png"
 
 const Header = () => {
     return (
-        <div>
-                <div className="navbar bg-base-100">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <label tabIndex={0} className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-      </label>
-      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
-      </ul>
-    </div>
-    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li tabIndex={0}>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
-    </ul>
-  </div>
-  <div className="navbar-end">
-    <a className="btn">Button</a>
-  </div>
-</div>
+        <div className="">
+              <div className="navbar bg-base-100 shadow ">
+                <div className="navbar-start">
+                  <a className="w-[200px] h-[100px] p-5"><img src={Logo} /></a>
+                  <div className="dropdown">
+                    <label tabIndex={0} className=" btn btn-ghost lg:hidden">
+                      <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 " fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className=" menu menu-lx dropdown-content mt-5 p-5 bg-base-100 ">
+                    <li><a className=" mx-auto">Home</a></li>
+                    <li><a className=" mx-auto">Privacy Policy</a></li>
+                    <li><a className=" mx-auto">Terms Condition</a></li>
+                    <li><a className=" mx-auto">About Us</a></li>
+                    <li><a className=" mx-auto">Contact Us</a></li>
+                    <li><a className=" mx-auto">Advertise</a></li>
+                      <li><a className=" btn-secondary m-2 py-3 px-5 ">Join Now</a></li>
+                    </ul>
+                  </div>
+               
+                </div>
+                <div className="navbar-center mt-6 ml-10 hidden lg:flex">
+                  <ul className="menu menu-horizontal">
+                    <li><a className=" ">Home</a></li>
+                    <li><a className=" ">Privacy Policy</a></li>
+                    <li><a className=" ">Terms Condition</a></li>
+                    <li><a className=" ">About Us</a></li>
+                    <li><a className=" ">Contact Us</a></li>
+                    <li><a className=" ">Advertise</a></li>
+                    <li><a className=" btn-secondary py-3 px-5">Join Now</a></li>
+                  </ul>
+                </div>
+                    <div className="navbar-end">
+                    </div>
+              </div>
         </div>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,30 @@ export default {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["light", "dark", "cupcake"],
+    themes: [ "light",
+      {
+        mytheme: {
+        
+"primary": "#C82333",
+        
+"secondary": "#d926a9",
+        
+"accent": "#1fb2a6",
+        
+"neutral": "#2a323c",
+        
+"base-100": "#1d232a",
+        
+"info": "#3abff8",
+        
+"success": "#36d399",
+        
+"warning": "#fbbd23",
+        
+"error": "#f87272",
+        },
+      },
+    ],
   },
+
 }
